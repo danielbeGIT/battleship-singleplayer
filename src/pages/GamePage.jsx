fca4c91883632bcc0a5bf56f2f24627052a33225
Daniel <Daniel_Be_95@msn.com>
2023-03-09 18:33:03 +0900

    small changes on homepage and style files

--- src/pages/GamePage.jsx
+++ src/pages/GamePage.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Header from '../components/Header'
 import PlayField from '../components/PlayField'
-import "../assets/styles/GamePage.scss"
 
 const GamePage = () => {
     return (
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Button } from 'react-bootstrap/'
 import { Link } from 'react-router-dom'
-import "../assets/styles/HomePage.scss"
 
 const HomePage = () => {
     return (
