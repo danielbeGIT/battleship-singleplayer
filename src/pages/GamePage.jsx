3e33cd68abf5631e2f1835a1dfbb7c5790e564c8
Daniel <Daniel_Be_95@msn.com>
2023-04-04 18:43:28 +0900

    handle winner/loser, working on randomizing ships

--- src/pages/GamePage.jsx
+++ src/pages/GamePage.jsx
@@ -1,11 +1,16 @@
 // React
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
 import { Button } from "react-bootstrap/"
 
 // Components
 import MyPlayField from "../components/MyPlayField"
 import EnemyField from "../components/EnemyField"
+import {randomizeShips, myShips} from "../components/Randomizer"
+
+// Helpers
+import {checkArrayOfIds} from "../helpers/Filter"
+import {resetShips} from "../helpers/ResetShips"
 
 const GamePage = () => {
     const [gameStarted, setGameStarted] = useState(false)
@@ -28,12 +33,17 @@ const GamePage = () => {
     }
 
     // Handle winner/loser
-    const handleWinner = () => {
-        setWinner(true)
+    const handleResult = () => {
+        if (!myShips === 0) {
+            setWinner(true)
+        } else {
+            setLoser(true)
+        }
     }
-    const handleLoser = () => {
-        setLoser(true)
-    }
+
+    // useEffect(() => {
+    //     randomizeShips()
+    // }, [gameStarted])
 
     return (
         <div className="game_page">
@@ -41,17 +51,21 @@ const GamePage = () => {
                 <h2>Battleship Singleplayer</h2>
                 <div className="header_button">
                     <Button size="lg" as={Link} to="/">Give up</Button>
-                    <Button className="startButton" onClick={() => setGameStarted(true) } size="lg">Start the game</Button>
+                    <Button className="startButton" onClick={() => setGameStarted(gameStarted => !gameStarted) } size="lg">Start the game</Button>
                 </div>
             </div>
 
             {!gameStarted && <marquee behavior="alternate"><h1>You need to start the game...</h1></marquee>}
 
+            {winner && <marquee behavior="alternate"><h1>You are the winner!</h1></marquee>}
+
+            {loser && <marquee behavior="alternate"><h1>Too bad.. you lost</h1></marquee>}
+
             {gameStarted && (
                 <div className="playfield_section">
                     <div className="myField">
                         <div className="player_score">
-                            <h2 className={myTurn ? 'playersTurn' : ''}>You</h2>
+                            <h2>You</h2>
                             Ships left: {myShips}
                         </div>
                         <MyPlayField />
@@ -59,7 +73,7 @@ const GamePage = () => {
 
                     <div className="enemyField">
                         <div className="player_score">
-                            <h2 className={myTurn ? 'playersTurn' : ''}>Computer</h2>
+                            <h2>Computer</h2>
                             Ships left: {enemyShips}
                         </div>
                         <EnemyField onClick={handleBoxClick} myTurn={myTurn}/>
